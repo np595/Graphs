04414631ad8c7200b06a630b3cd24107cb904043
np595 <49132942+np595@users.noreply.github.com>
2020-04-13 10:54:39 -0700

    Update GridNode.java
    Comments/ Code Review

--- GridNode.java
+++ GridNode.java
@@ -1,4 +1,5 @@
 import java.util.*;
+//Side note, but if you want, you could make things easier and just set a visited value so if the node is visited then just set it to true
 public class GridNode {
     final int x;
     final int y;
